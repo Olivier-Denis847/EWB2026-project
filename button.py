65f9954defd5fc1810ea76cf5623507cde207328
Olivier-Denis847 <olivierphilippedenis@gmail.com>
2026-03-14 20:34:05 -0400

    Fixed play button glitches

--- button.py
+++ button.py
@@ -10,7 +10,8 @@ class Button:
     def draw(self, screen):
         pygame.draw.circle(screen, self.color, self.centre, self.radius)
         if self.icon_path:
-            icon = pygame.image.load(self.icon_path)
+            big_icon = pygame.image.load(self.icon_path).convert_alpha()
+            icon = pygame.transform.smoothscale(big_icon, (self.radius * 1.5, self.radius * 1.5))
             icon_rect = icon.get_rect(center=self.centre)
             screen.blit(icon, icon_rect)
     
@@ -18,10 +19,8 @@ class Button:
         x, y = pos
         cx, cy = self.centre
         distance = ((x - cx) ** 2 + (y - cy) ** 2) ** 0.5
-        if (distance <= self.radius):
-            self.initial = not self.initial
-            return True
-        return False
+        return distance <= self.radius
 
     def update_icon(self, new_icon_path):
         self.icon_path = new_icon_path
+        self.initial = not self.initial
--- main.py
+++ main.py
@@ -8,6 +8,7 @@ pygame.mixer.init()
 WIDTH = 800
 HEIGHT = 600
 PLAY_BUTTON = button.Button((0, 255, 0), (WIDTH // 2, HEIGHT // 2), 50, 'assets/images/play_button.png')
+MAX_TIME = 5000
 
 screen = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption("My Pygame Project")
@@ -15,6 +16,8 @@ pygame.display.set_caption("My Pygame Project")
 clock = pygame.time.Clock()
 
 running = True
+timer = 0
+position = 0
 while running:
 
     for event in pygame.event.get():
@@ -24,12 +27,25 @@ while running:
             if PLAY_BUTTON.is_clicked(event.pos):
                 if not PLAY_BUTTON.initial:
                     pygame.mixer.music.load('assets/audio/Audio-Demo1.mp3')
-                    pygame.mixer.music.play(-1, 80)
+                    pygame.mixer.music.play(-1, 80 + (position / 1000))
                     PLAY_BUTTON.update_icon('assets/images/pause_button.png')
+                    timer = pygame.time.get_ticks()
+                    if position:
+                        timer -= position
                 else:
                     pygame.mixer.music.pause()
                     PLAY_BUTTON.update_icon('assets/images/play_button.png')
-
+                    position = pygame.time.get_ticks()
+                    if timer:
+                        position -= timer
+    if timer:
+        elapsed_time = (pygame.time.get_ticks() - timer)
+        if elapsed_time >= MAX_TIME:
+            pygame.mixer.music.stop()
+            PLAY_BUTTON.update_icon('assets/images/play_button.png')
+            timer = 0
+            position = 0
+        
     screen.fill((30, 30, 30))
 
     PLAY_BUTTON.draw(screen)
