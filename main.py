31e35767ba8348a0cd97d2e4939dfa5837a32177
Olivier-Denis847 <olivierphilippedenis@gmail.com>
2026-03-15 10:56:02 -0400

    Made a home screen

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 import pygame
+import random
+import string
 import sys
 from src import button
 from src import textbox
@@ -17,11 +19,13 @@ font_main = pygame.font.SysFont('Arial', 24)
 font_large = pygame.font.SysFont('Arial', 30, bold=True)
 font_small = pygame.font.SysFont('Arial', 18)
 
-STATE = 'main'
+STATE = 'title'
 
 PLAY_BUTTON = button.Button(HIGHLIGHT_COLOR, (3*WIDTH // 4 + 50, HEIGHT // 4 + 40), 20, PLAY_ICON_PATH)
 INPUT_BOX = textbox.TextBox(WIDTH // 3 + 50, HEIGHT * 2 // 3, WIDTH // 2 - 30, 50, font_main)
 BACK_BUTTON = pygame.Rect(WIDTH // 2 - 100, HEIGHT * 2 // 3, 200, 40)
+START_BUTTON = pygame.Rect(WIDTH // 2 - 100, HEIGHT // 2 - 25, 200, 50)
+ROOM_CODE_BOX = textbox.TextBox(WIDTH // 2 - 100, HEIGHT * 3 // 4 - 25, 200, 40, font_main)
 
 songs = []
 with open('audio_clips.txt', 'r') as f:
@@ -39,6 +43,7 @@ timer = 0
 position = 0
 score = 0
 final_answer = ''
+room_code = ''
 while running:
 
     for event in pygame.event.get():
@@ -65,6 +70,7 @@ while running:
                 users[0].update_score(score)
                 STATE = 'results'
                 final_answer = guess
+
         elif STATE == 'results':
             if event.type == pygame.MOUSEBUTTONDOWN:
                 if BACK_BUTTON.collidepoint(event.pos):
@@ -76,6 +82,17 @@ while running:
                     timer = 0
                     PLAY_BUTTON.update_icon(PLAY_ICON_PATH)
                     PLAY_BUTTON.initial = False
+        
+        elif STATE == 'title':
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                if START_BUTTON.collidepoint(event.pos):
+                    STATE = 'main'
+                    room_code = ''.join(random.choices(string.ascii_letters, k=5))
+                    room_code = room_code.upper()
+            code = ROOM_CODE_BOX.handle_event(event)
+            if code is not None:
+                room_code = code.upper()
+                STATE = 'main'
     
     if PLAY_BUTTON.initial:
         position = (pygame.time.get_ticks() - timer) if timer else 0
@@ -116,6 +133,9 @@ while running:
         screen.blit(game_text, (WIDTH*5 // 8 - game_text.get_width() // 2, HEIGHT // 4 - 100))
         screen.blit(prompt_text, (WIDTH*5 // 8 - prompt_text.get_width() // 2, HEIGHT // 2))
 
+        room_text = font_small.render(f'Room Code: {room_code}', True, FONT_COLOR)
+        screen.blit(room_text, (10, HEIGHT - room_text.get_height() - 10))
+
     elif STATE == 'results':
         answer_text = font_main.render(f'Answer: {song.answer}', True, FONT_COLOR)
         guess_text = font_main.render(f'Guess: {final_answer}', True, FONT_COLOR)
@@ -124,10 +144,26 @@ while running:
         screen.blit(guess_text, (WIDTH // 2 - guess_text.get_width() // 2, HEIGHT // 2 - guess_text.get_height() - 20))
         screen.blit(result_text, (WIDTH // 2 - result_text.get_width() // 2, HEIGHT // 2 - result_text.get_height() // 2))
 
-        pygame.draw.rect(screen, HIGHLIGHT_COLOR, BACK_BUTTON, border_radius=20)
+        pygame.draw.rect(screen, HIGHLIGHT_COLOR, BACK_BUTTON, border_radius=15)
         back_text = font_main.render('Back', True, DARK_COLOR)
-        screen.blit(back_text, 
-                    (BACK_BUTTON.centerx - back_text.get_width() // 2, BACK_BUTTON.centery - back_text.get_height() // 2))
+        screen.blit(back_text, (BACK_BUTTON.centerx - back_text.get_width() // 2, 
+                                BACK_BUTTON.centery - back_text.get_height() // 2))
+
+    elif STATE == 'title':
+        title_text = font_large.render('BrainLabel', True, FONT_COLOR)
+        screen.blit(title_text, (WIDTH // 2 - title_text.get_width() // 2, HEIGHT // 3 - title_text.get_height() // 2))
+
+        pygame.draw.rect(screen, HIGHLIGHT_COLOR, START_BUTTON, border_radius=20)
+        start_text = font_main.render('Start Game', True, DARK_COLOR)
+        screen.blit(start_text, (START_BUTTON.centerx - start_text.get_width() // 2, 
+                                START_BUTTON.centery - start_text.get_height() // 2))
+        
+        second_text = font_large.render('or', True, FONT_COLOR)
+        screen.blit(second_text, (WIDTH // 2 - second_text.get_width() // 2, HEIGHT // 2 + 45))
+        room_text = font_main.render('Join Room', True, FONT_COLOR)
+        screen.blit(room_text, (WIDTH // 2 - room_text.get_width() // 2, HEIGHT // 2 + 85))
+
+        ROOM_CODE_BOX.draw(screen)
     pygame.display.flip()
     clock.tick(60)
 
